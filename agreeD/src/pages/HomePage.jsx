c116719eaad9fe3d5e6b38fabc715c6e31f6d73c
Sameer Kumar <2006sameeer2009@gmail.com>
2025-01-26 17:37:52 +0530

    Add: Implement Sections component with content and styling; update Footer and HomePage layout

--- agreeD/src/pages/HomePage.jsx
+++ agreeD/src/pages/HomePage.jsx
@@ -2,12 +2,42 @@ import React from "react";
 import HeroSection from "../components/HomePage/sections/HeroSection";
 import Footer from "../components/HomePage/footer/Footer";
 import "./HomePage.css";
+import Sections from "../components/HomePage/sections/Sections";
 
 const HomePage = () => {
   return (
-    <div style={{ backgroundColor: "#000" }}>
-      <HeroSection />
+    <div style={{ backgroundColor: "#000", color: "#ffffff", position: "relative" }}>
+      <section id="hero-section">
+        <HeroSection />
+      </section>
+      <Sections />
       <Footer />
+
+      <div
+        style={{
+          position: "fixed",
+          top: "10%",
+          left: "2%",
+          padding: "20px",
+          backgroundColor: "#202121",
+          borderRadius: "10px",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <a style={{ color: "#fff" }} href="#hero-section">
+          Intro
+        </a>
+        <a style={{ color: "#fff" }} href="#limitations-section">
+          Limitations
+        </a>
+        <a style={{ color: "#fff" }} href="#iterative-deployment-section">
+          Iterative Deployment
+        </a>
+        <a style={{ color: "#fff" }} href="#setup-section">
+          Setup
+        </a>
+      </div>
     </div>
   );
 };
