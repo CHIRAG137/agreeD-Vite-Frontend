ad9aa8069df8b42e5c9d0f28b186e3453530023a
Sameer Kumar <2006sameeer2009@gmail.com>
2025-01-27 21:43:41 +0530

    feat: update OverviewTab to include clickable links for Heygen video and AgreeD chatbot

--- agreeD/src/components/contractDetails/OverviewTab.jsx
+++ agreeD/src/components/contractDetails/OverviewTab.jsx
@@ -13,25 +13,35 @@ const OverviewTab = ({ contractDetails }) => {
       ></video>
       <div>
         <span style={{ color: "orange" }}>Heygen Video Link:</span>{" "}
-        <span>
+        <a
+          href={`https://resource2.heygen.ai/video/${
+            contractDetails.heygenVideoId || "92f93fc53304401cad3f8426633d8cb2"
+          }/1280x720.mp4`}
+          target="_blank"
+        >
           {truncateWithDots(
             `https://resource2.heygen.ai/video/${
               contractDetails.heygenVideoId || "92f93fc53304401cad3f8426633d8cb2"
             }/1280x720.mp4`,
             50
           )}
-        </span>
+        </a>
       </div>
       <div>
         <span style={{ color: "orange" }}>AgreeD Chatbot Link:</span>{" "}
-        <span>
+        <a
+          href={`http://localhost:5173/chatbot/${
+            contractDetails.randomString || "372v6u5w1e4ju5t6i2m475u4i3g2i5r3"
+          }`}
+          target="_blank"
+        >
           {truncateWithDots(
-            `https://resource2.heygen.ai/video/${
-              contractDetails.heygenVideoId || "92f93fc53304401cad3f8426633d8cb2"
-            }/1280x720.mp4`,
+            `http://localhost:5173/chatbot/${
+              contractDetails.randomString || "372v6u5w1e4ju5t6i2m475u4i3g2i5r3"
+            }`,
             50
           )}
-        </span>
+        </a>
       </div>
     </div>
   );
